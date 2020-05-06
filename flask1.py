21fd6dc318ce63fde059601d970613b6cd3ca009
Fidranth <pavk13@outlook.com>
2020-05-06 16:49:33 -0600

    flask1.py file updated and added

---
+++ flask1.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Phil von Kaenel',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April May 5, 2020'
+    },
+    {
+        'author': 'Tiesha von Kaenel',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'May 5, 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
